05eeb816b43a324ae7af2866eac9149f44530b0a
Hayk04 <75264584+Hayk04@users.noreply.github.com>
2022-02-25 22:44:12 +0400

    Add files via upload

---
+++ editor.js
@@ -0,0 +1,15 @@
+var x = document.getElementById('agent')
+var k = document.getElementById('changer')
+var y = document.getElementById('teror')
+
+function myf(){
+	x.classList.toggle('terorist')
+	k.style.display = 'none'
+	y.style.display = 'block'
+}
+
+function myFunction(){
+	x.classList.toggle('terorist')
+	y.style.display = 'none'
+	k.style.display = 'block'
+}
